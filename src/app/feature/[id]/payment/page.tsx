5bfd9594ed161beccb71cbcfc6b0775eed1a942e
ikaychina <amaechinaikechukwu6@gmail.com>
2024-11-19 19:55:21 +0100

    Addition of Suspense, change of accent color and further UI improvement

--- src/app/feature/[id]/payment/page.tsx
+++ src/app/feature/[id]/payment/page.tsx
@@ -2,12 +2,12 @@ import FlutterwavePayment from "@/components/UI/Payments";
 import React from "react";
 
 export default async function page({ params }: { params: { id: string } }) {
-  let data = await fetch(
+  const data = await fetch(
     `${process.env.NEXT_PUBLIC_API}/contributions/${decodeURIComponent(
       params.id
     )}`
   );
-  let group = await data.json();
+  const group = await data.json();
 
   return (
     <div className="h-full flex items-center justify-center">
